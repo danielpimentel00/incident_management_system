cfba52a333b0302fd3530de4cd8d4806e3620934
DanielPimentel <danielpimentel19@gmail.com>
2026-04-12 15:41:48 -0400

    Adds API's Dockerfile

--- incident_management_system.API/Program.cs
+++ incident_management_system.API/Program.cs
@@ -14,6 +14,7 @@ using Serilog.Enrichers.Span;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddOpenTelemetry()
